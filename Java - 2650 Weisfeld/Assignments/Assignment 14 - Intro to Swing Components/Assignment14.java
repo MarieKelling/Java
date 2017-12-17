1b7fbc79c75839d0b629667fe5bbdb00ec7a277e
MarieKelling <mariekelling97@gmail.com>
2017-12-17 18:02:09 -0500

    Updates.

---
+++ Java - 2650 Weisfeld/Assignments/Assignment 14 - Intro to Swing Components/Assignment14.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+public class Assignment14
+{
+
+	public static void main (String[] args)
+	{
+		JFrame frame = new JFrame();
+		frame.setSize(400, 200);
+		
+		JButton button = new JButton("Marie Kelling");
+		button.setBounds(100, 50, 200, 40);
+		frame.add(button);
+		
+		
+		frame.setVisible(true);
+	
+	
+	}
+
+
+}
