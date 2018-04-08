2200704c69a993628068234fab31081ed7107532
MarieKelling <mariekelling97@gmail.com>
2018-04-08 10:43:26 -0400

    Lab 7 - add, find, traverse functionality
    Lab 7 - add, find, traverse functionality

--- Algorithms IT2660/Assignments/Chapter 7 - Binary Tree Search/BinaryTree.java
+++ Algorithms IT2660/Assignments/Chapter 7 - Binary Tree Search/BinaryTree.java
@@ -6,72 +6,102 @@ public class BinaryTree
 	{
 		Node nodeToAdd = new Node(key, name); 	
 		
-		//If tree is empty, create root node:  
+		//If tree is empty, create root node  
 		if(root == null)
 		{
 			root = nodeToAdd;
-         System.out.println(nodeToAdd.name + " has been added to the Family Tree."); 
-			return; 
+			System.out.println(nodeToAdd.name + " has been added to the Family Tree as the Root."); 
 		}
 		
-		//If tree is not empty
+		//If tree is not empty, recursively traverse the tree 
 		else
 		{
-          //Recursively traverse the tree to properly add the node
-		    traverseNode(root, nodeToAdd);		
-		
-		}
+			Node nodeOfFocus = root;
+			Node parent; 
+			
+			while (true)
+			{
+				parent = nodeOfFocus;
+				
+				 //Key of the node to be added is less than the parent node currently in focus
+				 if(nodeToAdd.key < nodeOfFocus.key)
+				{  
+					nodeOfFocus = nodeOfFocus.leftChild;
+						
+					 //Empty space found, place new node there 
+					 if(nodeOfFocus == null)
+					 {
+						parent.leftChild = nodeToAdd; 
+						return;
+					 }
+				}
+				//Key of the node to be added is more than the parent node currently in focus
+				else
+				{
+					nodeOfFocus = nodeOfFocus.rightChild;
+						
+					 //Empty space found, place new node there 
+					 if(nodeOfFocus == null)
+					 {     
+						parent.rightChild = nodeToAdd;
+						return;
+					 }
+				}		
+			}
+		}	
 	}
-   
-   private void traverseNode(Node nodeInFocus, Node nodeToAdd)
-   {
-      //The node to be added is less than the parent node currently in focus
-      if(nodeToAdd.key < nodeInFocus.key)
-      {  
-         //Empty space found, fill it w/ the new node
-         if(nodeInFocus.leftChild == null)
+         
+         public void inOrderTraversal(Node nodeOfFocus)
          {
-            nodeInFocus.leftChild = nodeToAdd;
-            System.out.println(nodeToAdd.name + 
-                               " has been added to the Family Tree as " + nodeInFocus.name + 
-                               "'s Left Child"); 
+            if (nodeOfFocus != null)
+			   {
+				//Traverse left node
+				inOrderTraversal(nodeOfFocus.leftChild);
+				
+				System.out.println(nodeOfFocus); 
+				
+				//Traverse right node
+				inOrderTraversal(nodeOfFocus.rightChild); 
+			   }
          }
-         //No Empty space yet, traverse again
-         else
-         {
-            traverseNode(nodeInFocus.leftChild, nodeToAdd);
-         }
-      }
-      
-      //The node to be added is more than the parent node currently in focus
-      else if (nodeToAdd.key > nodeInFocus.key)
-      {
-         //Empty space found, fill it w/ the new node
-         if(nodeInFocus.rightChild == null)
-         {
-            nodeInFocus.rightChild = nodeToAdd;
-            System.out.println(nodeToAdd.name + 
-                               " has been added to the Family Tree as " + nodeInFocus.name + 
-                               "'s Right Child"); 
- 
-         }
-         //No Empty space yet, traverse again
-         else
-         {
-            traverseNode(nodeInFocus.rightChild, nodeToAdd); 
-         }
-       }
-   }
+
+		public Node findNode(int key)
+		{
+			Node nodeOfFocus = root;
+			
+			while (nodeOfFocus.key != key)
+			{
+				if (key < nodeOfFocus.key)   {
+					
+					nodeOfFocus = nodeOfFocus.leftChild; 
+					
+				} else   {
+					
+					nodeOfFocus = nodeOfFocus.rightChild; 
+				}
+
+				if (nodeOfFocus == null)
+					return null;
+			}
+			return nodeOfFocus; 
+		}
 
 	public static void main(String [] args)
 	{
-		BinaryTree tree = new BinaryTree();
+		BinaryTree familyTree = new BinaryTree();
 		
-		tree.addNode(58, "James");
-		tree.addNode(56, "Sandra");
-		tree.addNode(26, "Nate");
-		tree.addNode(21, "Marie"); 
-      
+		  familyTree.addNode(56, "Sandra");
+		  familyTree.addNode(26, "Nate");
+		  familyTree.addNode(21, "Marie");
+		  familyTree.addNode(88, "Grandma");
+		  familyTree.addNode(90, "Grandpa"); 
+		  familyTree.addNode(80, "Gerdie");  
+		  
+		  familyTree.inOrderTraversal(familyTree.root); 
+        System.out.println();
+        
+        System.out.println("Find the Node with the key 88: "); 
+        System.out.println(familyTree.findNode(88)); 
       
 	}
 
