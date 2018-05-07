b9e2f3ca5f4f62ab2b8f8f7574df22b071c6bb17
MarieKelling <mariekelling97@gmail.com>
2018-05-07 11:13:59 -0400

    Updates
    Updates

--- Algorithms IT2660/Assignments/Chapter 9 - Final Project/Graph.java
+++ Algorithms IT2660/Assignments/Chapter 9 - Final Project/Graph.java
@@ -7,13 +7,13 @@ class ShortestPath
     // A utility function to find the vertex with minimum distance value,
     // from the set of vertices not yet included in shortest path tree
     static int V=9;
-    int minDistance(int dist[], Boolean sptSet[])
+    int minDistance(int dist[], Boolean spTreeSet[])
     {
         // Initialize min value
         int min = Integer.MAX_VALUE, min_index=-1;
  
         for (int v = 0; v < V; v++)
-            if (sptSet[v] == false && dist[v] <= min)
+            if (spTreeSet[v] == false && dist[v] <= min)
             {
                 min = dist[v];
                 min_index = v;
@@ -30,45 +30,40 @@ class ShortestPath
             System.out.println(i + " -------------> " + dist[i]);
     }
  
-    //Dijkstra's shortest path from a given source 
-    void dijkstra(int graph[][], int src)
+    //Dijkstra's algorithm to calculate the shortest path from a given source 
+    void dijkstra(int graph[][], int source)
     {
-        int dist[] = new int[V]; // The output array. dist[i] will hold
-                                 // the shortest distance from src to i
+        int dist[] = new int[V]; // The output array. dist[i] hold shortest distance from src to i
  
         // sptSet[i] will true if vertex i is included in shortest
         // path tree or shortest distance from src to i is finalized
-        Boolean sptSet[] = new Boolean[V];
+        Boolean spTreeSet[] = new Boolean[V];
  
-        // Initialize all distances as INFINITE and stpSet[] as false
         for (int i = 0; i < V; i++)
         {
             dist[i] = Integer.MAX_VALUE;
-            sptSet[i] = false;
+            spTreeSet[i] = false;
         }
  
-        // Distance of source vertex from itself is always 0
-        dist[src] = 0;
+        dist[source] = 0;
  
-        // Find shortest path for all vertices
+        // Find sp
         for (int count = 0; count < V-1; count++)
         {
             // Pick the minimum distance vertex from the set of vertices
-            // not yet processed. u is always equal to src in first
-            // iteration.
-            int u = minDistance(dist, sptSet);
+            // not yet processed. 
+            int u = minDistance(dist, spTreeSet);
  
-            // Mark the picked vertex as processed
-            sptSet[u] = true;
+            // Mark chosen vertex 
+            spTreeSet[u] = true;
  
-            // Update dist value of the adjacent vertices of the
-            // picked vertex.
+            // Update dist value of the chosen vertex of the adjacent verticex
             for (int v = 0; v < V; v++)
  
                 // Update dist[v] only if is not in sptSet, there is an
                 // edge from u to v, and total weight of path from src to
                 // v through u is smaller than current value of dist[v]
-                if (!sptSet[v] && graph[u][v]!=0 &&
+                if (!spTreeSet[v] && graph[u][v]!=0 &&
                         dist[u] != Integer.MAX_VALUE &&
                         dist[u]+graph[u][v] < dist[v])
                     dist[v] = dist[u] + graph[u][v];
